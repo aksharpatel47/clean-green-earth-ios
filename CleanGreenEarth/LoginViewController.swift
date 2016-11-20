6e2c1c2550a8e695fb891f0bd7f9dcc9275d1718
Akshar Patel <akshar.patel.47@gmail.com>
2016-11-21 00:30:32 +0530

    feat: Scaffold LoginVC

---
+++ CleanGreenEarth/LoginViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LoginViewController.swift
+//  CleanGreenEarth
+//
+//  Created by Akshar Patel on 19/11/16.
+//  Copyright © 2016 Akshar Patel. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+  
+  @IBOutlet weak var emailTextField: UITextField!
+  @IBOutlet weak var passwordTextField: UITextField!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    
+  }
+
+  @IBAction func login(_ sender: AnyObject) {
+    
+  }
+  
+  @IBAction func signup(_ sender: AnyObject) {
+    
+  }
+}
+
