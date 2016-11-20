522e1f3925cb246f0e0af34e19ce77b00a06a97c
Akshar Patel <akshar.patel.47@gmail.com>
2016-11-21 00:37:58 +0530

    feat: Scaffold SignupVC

---
+++ CleanGreenEarth/SignupViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SignupViewController.swift
+//  CleanGreenEarth
+//
+//  Created by Akshar Patel on 21/11/16.
+//  Copyright © 2016 Akshar Patel. All rights reserved.
+//
+
+import UIKit
+
+class SignupViewController: UIViewController {
+  
+  @IBOutlet weak var emailTextField: UITextField!
+  @IBOutlet weak var passwordTextField: UITextField!
+  @IBOutlet weak var retypePasswordTextField: UITextField!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  @IBAction func signup(_ sender: AnyObject) {
+    
+  }
+}
