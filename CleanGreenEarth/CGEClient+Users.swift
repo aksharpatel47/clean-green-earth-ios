e0ace526e714e48a30dccfb1de24586f154135f1
Akshar Patel <akshar.patel.47@gmail.com>
2016-12-27 19:57:52 +0530

    fix: Abstract authentication logic inside cge-client

--- CleanGreenEarth/CGEClient+Users.swift
+++ CleanGreenEarth/CGEClient+Users.swift
@@ -7,8 +7,58 @@
 //
 
 import Foundation
+import FirebaseAuth
 
 extension CGEClient {
+  func signUp(withEmail email: String, password: String, name: String, completionHandler: @escaping ((FIRUser?, Error?) -> Void)) {
+    
+    let payload = ["email": email, "password": password, "name": name]
+    request(method: .POST, path: Paths.users, queryString: nil, jsonBody: payload) {
+      data, error in
+      
+      guard error == nil else {
+        completionHandler(nil, error)
+        return
+      }
+      
+      FIRAuth.auth()?.signIn(withEmail: email, password: password, completion: completionHandler)
+    }
+  }
+  
+  func signIn(withEmail email: String, password: String, completionHandler: @escaping ((FIRUser?, Error?) -> Void)) {
+    FIRAuth.auth()?.signIn(withEmail: email, password: password, completion: completionHandler)
+  }
+  
+  func signIn(withFacebookToken token: String, completionHandler: @escaping ((FIRUser?, Error?) -> Void)) {
+    
+    let fbCredentials = FIRFacebookAuthProvider.credential(withAccessToken: token)
+    
+    FIRAuth.auth()?.signIn(with: fbCredentials) {
+      user, error in
+      
+      guard let user = user,
+        let name = user.displayName,
+        let photoURL = user.photoURL,
+        error == nil else {
+        completionHandler(nil, error)
+        return
+      }
+      
+      let payload = ["name": name, "imageURL": photoURL.absoluteString]
+      
+      self.request(method: .POST, path: Paths.users, queryString: nil, jsonBody: payload) {
+        data, error in
+        
+        guard error == nil else {
+          completionHandler(nil, error)
+          return
+        }
+        
+        completionHandler(user, nil)
+      }
+    }
+  }
+  
   func updateName(name: String, completionHandler: @escaping ((Any?, Error?) -> Void)) {
     let payload = [
       "name": name
